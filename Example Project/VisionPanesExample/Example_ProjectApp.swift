c013ea43a29d7360129ed51f99b92893e2491d2d
Laksh Chakraborty <lakshc2010@gmail.com>
2024-03-06 01:42:18 -0500

    chore: example changes

--- Example Project/VisionPanesExample/Example_ProjectApp.swift
+++ Example Project/VisionPanesExample/Example_ProjectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Example_ProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            SameEdge()
+            ContentView()
         }
         .windowStyle(.plain)
     }
